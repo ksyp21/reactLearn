1efb0da931f1b56172ad45276a67c19c937467fc
ksyp21 <ksypkarki@gmail.com>
2023-12-03 00:25:58 +0545

    initial commit

---
+++ CMSproject/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <h2>Bye World</h2>
+      <p style={{ color: "red" }}>By Kashyap</p>
+    </>
+  )
+}
+
+export default App
